ad4293d83ae033c1d503bb3660136ab484b71daa
Diana <kdv012025@gmail.com>
2022-12-15 19:56:58 +0400

    commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let tabs = document.querySelectorAll('.tabheader__item');
+    let tabsContent = document.querySelectorAll('.tabcontent');
+    let tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i) {
+        tabsContent[i].classList.add('show');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('.tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
